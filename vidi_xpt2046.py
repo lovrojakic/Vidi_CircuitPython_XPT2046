a45727f8b8377a60e5215ef3fe6ff2d59d0432b0
Lovro Jakic <lovro.jakicc@gmail.com>
2024-08-22 14:11:55 +0200

    Initial commit

---
+++ vidi_xpt2046.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2024 Lovro Jakic for VIDI
+#
+# SPDX-License-Identifier: MIT
+"""
+`vidi_xpt2046`
+================================================================================
+
+CircuitPython driver library for the XPT2046 touch screen controller.
+
+
+* Author(s): Lovro Jakic
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/lovrojakic/Vidi_CircuitPython_XPT2046.git"
